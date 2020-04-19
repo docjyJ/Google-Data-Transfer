550868b4962f203b6b5c2545b8a2e18da0b74bc7
docjyJ <docteurjeanyves@gmail.com>
2020-04-19 12:09:09 +0200

    Add run.bat

---
+++ run.bat
@@ -0,0 +1,8 @@
+if exist src/main/resources/client_secret.json (
+    gradlew.bat build
+    java -jar build/libs/GoogleTransfer-BETA.jar
+) else (
+    echo no client_secret.json found in src/main/resources/
+)
+pause
+
