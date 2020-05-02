6cea0fe8be27f60aebd0a543267f432bfac4b61d
docjyJ <docteurjeanyves@gmail.com>
2020-05-02 18:56:53 +0200

    update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
+@echo off
 if exist src/main/resources/client_secret.json (
     gradlew.bat build
     java -jar build/libs/GoogleTransfer-BETA.jar
--- src/main/java/fr/docjyJ/googleTransfer/Utils/GoogleTransfer.java
+++ src/main/java/fr/docjyJ/googleTransfer/Utils/GoogleTransfer.java
@@ -7,18 +7,17 @@ import com.google.gson.GsonBuilder;
 
 import java.awt.*;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 public class GoogleTransfer {
     protected static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     protected static final String CLIENT_SECRETS = "client_secret.json";
     protected static final String APPLICATION_NAME = "Google Transfer";
     protected static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    protected static final Collection<String> SCOPES = List.of(
+    protected static final Collection<String> SCOPES = Arrays.asList(
             "https://www.googleapis.com/auth/youtube",
             "https://www.googleapis.com/auth/calendar",
             "https://www.googleapis.com/auth/drive",
@@ -42,8 +41,8 @@ public class GoogleTransfer {
     }
     public GoogleTransfer generate() throws IOException {
         this.file.createNewFile();
-        PrintWriter writer = new PrintWriter(this.file, StandardCharsets.UTF_8);
-        writer.println(this.toJson());
+        FileWriter writer = new FileWriter(this.file);
+        writer.write(this.toJson());
         writer.close();
         return this;
     }
