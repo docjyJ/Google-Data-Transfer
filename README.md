f886cb6a30e2071fe9a1ecd021bbb3fa15ee57a9
docjyJ <docteurjeanyves@gmail.com>
2020-05-05 15:57:22 +0200

    Update App

--- README.md
+++ README.md
@@ -2,31 +2,38 @@
 Transfer Google data from one account to another.
 
 ## Transferable data and service
-* YouTube
+* YouTube **(works, but API quota's is limited)**
     * liked videos
     * disliked videos
     * subscribed channels
     * playlists
-* Google Calendar
+* Google Calendar **(some timeout problem with API)**
     * calendar and events
-* Contacts **(WIP)**
+
+### Work in progress
+* Contacts
     * contacts
-* Gmail **(WIP)**
+* Gmail
     * filters
     * settings
-* Google Drive **(WIP)**
+* Google Drive
+    * clone files
+* Google Fit
     * clone files
 
 ## Prerequisite
-Have a client_secrets.json file for Google OAuth 2.0 with apis :
-- YouTube Data API v3
-- Contacts API
-- Google Calendar API
+Have a Client Secrets Json file for Google OAuth 2.0 with apis :
 - Gmail API
+- Google Calendar API
 - Google Drive API
+- People API
+- YouTube Data API v3
 > More information : https://developers.google.com/identity/protocols/oauth2
 
 ## Install and Run
 1. Download the project
 2. Put the `client_secrets.json` file in `src/main/resources/`
 3. Run file `run.bat`
+
+## Picture Example
+![](Google%20Data%20Transfer.png)
--- src/main/java/fr/docjyJ/googleTransfer/app/Main.java
+++ src/main/java/fr/docjyJ/googleTransfer/app/Main.java
@@ -3,10 +3,8 @@ package fr.docjyJ.googleTransfer.app;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.youtube.model.PlaylistItem;
 import com.google.api.services.youtube.model.Subscription;
-import fr.docjyJ.googleTransfer.api.Services.calendar.CalendarElement;
 import fr.docjyJ.googleTransfer.api.Services.calendar.CalendarItemElement;
 import fr.docjyJ.googleTransfer.api.Services.youtube.PlaylistElement;
-import fr.docjyJ.googleTransfer.api.Services.youtube.YoutubeElement;
 import fr.docjyJ.googleTransfer.api.Utils.Service;
 
 import javax.swing.*;
@@ -15,23 +13,32 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Main extends JFrame {
-    private Service serviceOld;
-    private Service serviceNew;
+    private final Service serviceOld;
+    private final Service serviceNew;
+    private List<Setting> settings;
 
+    //main/start
     public static void main(String[] args) throws Exception {
-        new Main();
+        new Main().start();
+    }
+    public void start(){
+        readActivity();
     }
 
     //Constructor
     Main() throws Exception {
+        consoleView();
         setTitle(Lang.APPLICATION_NAME);
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
-        setConsole();
 
         JOptionPane.showMessageDialog(null,
                 Lang.FIRST_STEP,
@@ -43,28 +50,128 @@ class Main extends JFrame {
                 Lang.APPLICATION_NAME,
                 JOptionPane.INFORMATION_MESSAGE);
         this.serviceNew = new Service();
-        setNext("first");
     }
 
-    //Setters
-    private void setNext(String now) {
-        setConsole();
-        if (now.equals("first"))
-            try{
-                setYoutube();
-            } catch (IOException e) {
-                showError(e);
-                setNext("youtube");
+    //Activity
+    private void readActivity(){
+        consoleView();
+        settings = Arrays.asList(
+                new Setting("calendar"),
+                new Setting("like"),
+                new Setting("dislike"),
+                new Setting("subscription"),
+                new Setting("playlist"));
+        for (Setting setting: settings) {
+            switch (setting.name){
+                case "calendar" :
+                    setting.value = showQuestion(Lang.ASK_CALENDARS);
+                    break;
+                case "like" :
+                    setting.value = showQuestion(Lang.ASK_LIKE);
+                    break;
+                case "dislike" :
+                    setting.value = showQuestion(Lang.ASK_DISLIKE);
+                    break;
+                case "subscription" :
+                    setting.value = showQuestion(Lang.ASK_SUBSCRIPTION);
+                    break;
+                case "playlist" :
+                    setting.value = showQuestion(Lang.ASK_PLAYLIST);
+                    break;
             }
-        if (now.equals("youtube"))
-            try{
-                setCalendar();
-            } catch (IOException e) {
-                showError(e);
-                setNext("calendar");
+        }
+        for (Setting setting: settings) {
+            if(setting.value){
+                try{
+                    switch (setting.name){
+                        case "calendar" :
+                            serviceNew.getCalendar().readCalendars();
+                            serviceOld.getCalendar().readCalendars();
+                            break;
+                        case "like" :
+                            serviceNew.getYoutube().readLike();
+                            serviceOld.getYoutube().readLike();
+                            break;
+                        case "dislike" :
+                            serviceNew.getYoutube().readDislike();
+                            serviceOld.getYoutube().readDislike();
+                            break;
+                        case "subscription" :
+                            serviceNew.getYoutube().readSubscriptions();
+                            serviceOld.getYoutube().readSubscriptions();
+                            break;
+                        case "playlist" :
+                            serviceNew.getYoutube().readPlaylist();
+                            serviceOld.getYoutube().readPlaylist();
+                            break;
+                    }
+                } catch (IOException e) {
+                    setting.value = false;
+                    showError(e);
+                }
+
             }
+
+        }
+        treeView(arg0 -> putActivity());
     }
-    private void setMainPanel(Component center, Component south){
+    private void putActivity(){
+        consoleView();
+        for (Setting entry: settings) {
+            if(entry.value){
+                try{
+                    switch (entry.name){
+                        case "calendar" :
+                            serviceNew.getCalendar()
+                                    .putCalendars(serviceOld.getCalendar().getCalendars())
+                                    .readCalendars();
+                            break;
+                        case "like" :
+                            serviceNew.getYoutube()
+                                    .putLike(serviceOld.getYoutube().getLikes())
+                                    .readLike();
+                            break;
+                        case "dislike" :
+                            serviceNew.getYoutube()
+                                    .putDislike(serviceOld.getYoutube().getDislikes())
+                                    .readDislike();
+                            break;
+                        case "subscription" :
+                            serviceNew.getYoutube()
+                                    .putSubscriptions(serviceOld.getYoutube().getSubscriptions())
+                                    .readSubscriptions();
+                            break;
+                        case "playlist" :
+                            serviceNew.getYoutube()
+                                    .putPlaylist(serviceOld.getYoutube().getPlaylists())
+                                    .readPlaylist();
+                            break;
+                    }
+                } catch (IOException e) {
+                    showError(e);
+                }
+
+            }
+
+        }
+        treeView(arg1 -> consoleView());
+    }
+
+    //View
+    private void consoleView() {
+        JTextArea textArea = new JTextArea(50, 10);
+        PrintStream printStream = new PrintStream(new Console(textArea));
+        System.setOut(printStream);
+        System.setErr(printStream);
+        setView(textArea, null);
+    }
+    private void treeView(ActionListener btnAction) {
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, treeComponent(serviceOld), treeComponent(serviceNew));
+        JButton okButton = new JButton(Lang.NEXT);
+        okButton.addActionListener(btnAction);
+        setView(split, okButton);
+    }
+    private void setView(Component center, Component south){
         setVisible(false);
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
@@ -73,19 +180,93 @@ class Main extends JFrame {
         this.setContentPane(new JScrollPane(mainPanel));
         setVisible(true);
     }
-    private void setConsole() {
-        JTextArea textArea = new JTextArea(50, 10);
-        PrintStream printStream = new PrintStream(new Console(textArea));
-        System.setOut(printStream);
-        System.setErr(printStream);
-        setMainPanel(textArea, null);
+
+    //Component
+    private Component treeComponent(Service service) {
+        Map<String, Boolean> settings = new HashMap<>();
+        for (Setting setting : this.settings)
+            settings.put(setting.name, setting.value);
+        DefaultMutableTreeNode tree = new DefaultMutableTreeNode(Lang.APPLICATION_NAME);
+
+        //Calendar
+        if (settings.get("calendar")){
+            DefaultMutableTreeNode calendars = new DefaultMutableTreeNode(Lang.CALENDAR);
+            for (CalendarItemElement calendarEntry : service.getCalendar().getCalendars()) {
+                DefaultMutableTreeNode calendar = new DefaultMutableTreeNode(calendarEntry.getCalendar().getSummary());
+                for (Event eventEntry : calendarEntry.getEvents()) {
+                    DefaultMutableTreeNode event = new DefaultMutableTreeNode(eventEntry.getSummary());
+                    calendar.add(event);
+                }
+                calendars.add(calendar);
+            }
+            tree.add(calendars);
+        }
+
+        //Youtube
+        if (settings.get("like")||settings.get("dislike")||settings.get("subscription")||settings.get("playlist")) {
+            DefaultMutableTreeNode youtube = new DefaultMutableTreeNode(Lang.YOUTUBE);
+            //like
+            if (settings.get("like")) {
+                DefaultMutableTreeNode likes = new DefaultMutableTreeNode(Lang.LIKES);
+                for (String likeEntry : service.getYoutube().getLikes()) {
+                    DefaultMutableTreeNode like = new DefaultMutableTreeNode(likeEntry);
+                    likes.add(like);
+                }
+                youtube.add(likes);
+            }
+            //dislike
+            if (settings.get("dislike")) {
+                DefaultMutableTreeNode dislikes = new DefaultMutableTreeNode(Lang.DISLIKES);
+                for (String dislikeEntry : service.getYoutube().getDislikes()) {
+                    DefaultMutableTreeNode dislike = new DefaultMutableTreeNode(dislikeEntry);
+                    dislikes.add(dislike);
+                }
+                youtube.add(dislikes);
+            }
+            //subscription
+            if (settings.get("subscription")) {
+                DefaultMutableTreeNode subscriptions = new DefaultMutableTreeNode(Lang.SUBSCRIPTIONS);
+                for (Subscription subscriptionEntry : service.getYoutube().getSubscriptions()) {
+                    DefaultMutableTreeNode subscription = new DefaultMutableTreeNode(subscriptionEntry.getSnippet().getResourceId().getChannelId());
+                    subscriptions.add(subscription);
+                }
+                youtube.add(subscriptions);
+            }
+            //playlist
+            if (settings.get("playlist")) {
+                DefaultMutableTreeNode playlists = new DefaultMutableTreeNode(Lang.PLAYLISTS);
+                for (PlaylistElement playlistEntry : service.getYoutube().getPlaylists()) {
+                    DefaultMutableTreeNode playlist = new DefaultMutableTreeNode(playlistEntry.getPlaylist().getSnippet().getTitle());
+                    for (PlaylistItem itemEntry : playlistEntry.getItems()) {
+                        DefaultMutableTreeNode item = new DefaultMutableTreeNode(itemEntry.getSnippet().getResourceId().getVideoId());
+                        playlist.add(item);
+                    }
+                    playlists.add(playlist);
+                }
+                youtube.add(playlists);
+            }
+            tree.add(youtube);
+        }
+
+        //Panel
+        JPanel accountPanel = new JPanel();
+        accountPanel.setLayout(new BorderLayout());
+        accountPanel.add(
+                new JLabel(new ImageIcon(service.getUserPhoto())),
+                BorderLayout.WEST);
+        accountPanel.add(
+                new JLabel(
+                        "<html><font size=\"+2\">"+service.getUserName()+"</font><br/><i>"+service.getUserMail()+"</i></html>",
+                        SwingConstants.CENTER),
+                BorderLayout.CENTER);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(accountPanel, BorderLayout.NORTH);
+        mainPanel.add(new JTree(tree), BorderLayout.CENTER);
+        return mainPanel;
     }
-    private void setTreeView(JTree treeOld, JTree treeNew,ActionListener btnAction) {
-        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, treeOld, treeNew);
-        JButton okButton = new JButton(Lang.NEXT);
-        okButton.addActionListener(btnAction);
-        setMainPanel(split, okButton);
-    }
+
+    //Dialog Box
     private void showError(Exception e){
         JOptionPane.showMessageDialog(null,
                 e.getMessage(),
@@ -93,8 +274,8 @@ class Main extends JFrame {
                 JOptionPane.ERROR_MESSAGE);
 
     }
-    private int showQuestion(String message){
-        return JOptionPane.showConfirmDialog(null,
+    private boolean showQuestion(String message){
+        return 0 == JOptionPane.showConfirmDialog(null,
                 message,
                 Lang.APPLICATION_NAME,
                 JOptionPane.YES_NO_OPTION,
@@ -102,142 +283,18 @@ class Main extends JFrame {
 
     }
 
-
-    //Calendar
-    private void setCalendar() throws IOException {
-        if (showQuestion(Lang.ASK_CALENDAR) != 0) {
-            throw new IOException("Refused");
+    //Settings
+    private static class Setting {
+        public String name;
+        public boolean value;
+        public Setting(String type) {
+            this.name = type;
+            this.value = false;
         }
-        serviceOld.getCalendar().readAll();
-        serviceNew.getCalendar().readAll();
-        setTreeView(
-                setTreeCalendar(serviceOld.getCalendar()),
-                setTreeCalendar(serviceNew.getCalendar()),
-                arg0 -> {
-                    int calendar = showQuestion(Lang.ASK_CALENDARS);
-                    setConsole();
-                    if(calendar == 0) {
-                        try {
-                            serviceNew.getCalendar()
-                                    .putCalendars(serviceOld.getCalendar().getCalendars())
-                                    .readCalendars();
-                        } catch (Exception e) {
-                            showError(e);
-                        }
-                    }
-                    setTreeView(
-                            setTreeCalendar(serviceOld.getCalendar()),
-                            setTreeCalendar(serviceNew.getCalendar()),
-                            arg1 -> setNext("calendar"));
-
-                });
-    }
-    private JTree setTreeCalendar(CalendarElement calendarsEntry) {
-        DefaultMutableTreeNode calendars = new DefaultMutableTreeNode(Lang.CALENDAR);
-        for (CalendarItemElement calendarEntry :calendarsEntry.getCalendars()) {
-            DefaultMutableTreeNode calendar = new DefaultMutableTreeNode(calendarEntry.getCalendar().getSummary());
-            for (Event eventEntry: calendarEntry.getEvents()) {
-                DefaultMutableTreeNode event = new DefaultMutableTreeNode(eventEntry.getSummary());
-                calendar.add(event);
-            }
-            calendars.add(calendar);
-        }
-        return new JTree(calendars);
     }
 
-    //YouTube
-    private void setYoutube() throws IOException {
-        if (showQuestion(Lang.ASK_YOUTUBE) != 0) {
-            throw new IOException("Refused");
-        }
-        serviceNew.getYoutube().readAll();
-        serviceOld.getYoutube().readAll();
-        setTreeView(
-                setTreeYoutube(serviceOld.getYoutube()),
-                setTreeYoutube(serviceNew.getYoutube()),
-                arg0 -> {
-                    int like = showQuestion(Lang.ASK_LIKE);
-                    int dislike = showQuestion(Lang.ASK_DISLIKE);
-                    int subscription = showQuestion(Lang.ASK_SUBSCRIPTION);
-                    int playlist = showQuestion(Lang.ASK_PLAYLIST);
-                    setConsole();
-                    if(like == 0) {
-                        try {
-                            serviceNew.getYoutube()
-                                    .putLike(serviceOld.getYoutube().getLikes())
-                                    .readLike();
-                        } catch (IOException e) {
-                            showError(e);
-                        }
-                    }
-                    if(dislike == 0) {
-                        try {
-                            serviceNew.getYoutube()
-                                    .putDislike(serviceOld.getYoutube().getDislikes())
-                                    .readDislike();
-                        } catch (IOException e) {
-                            showError(e);
-                        }
-                    }
-                    if(subscription == 0) {
-                        try {
-                            serviceNew.getYoutube()
-                                    .putSubscriptions(serviceOld.getYoutube().getSubscriptions())
-                                    .readSubscriptions();
-                        } catch (IOException e) {
-                            showError(e);
-                        }
-                    }
-                    if(playlist == 0) {
-                        try {
-                            serviceNew.getYoutube()
-                                    .putPlaylist(serviceOld.getYoutube().getPlaylists())
-                                    .readPlaylist();
-                        } catch (IOException e) {
-                            showError(e);
-                        }
-                    }
-                    setTreeView(
-                            setTreeYoutube(serviceOld.getYoutube()),
-                            setTreeYoutube(serviceNew.getYoutube()),
-                            arg1 -> setNext("youtube"));
 
-                });
-    }
-    private JTree setTreeYoutube(YoutubeElement youtubeEntry) {
-        DefaultMutableTreeNode youtube = new DefaultMutableTreeNode(Lang.YOUTUBE);
-        //like
-        DefaultMutableTreeNode likes = new DefaultMutableTreeNode(Lang.LIKES);
-        for (String likeEntry : youtubeEntry.getLikes()) {
-            DefaultMutableTreeNode like = new DefaultMutableTreeNode(likeEntry);
-            likes.add(like);
-        }
-        youtube.add(likes);
-        //dislike
-        DefaultMutableTreeNode dislikes = new DefaultMutableTreeNode(Lang.DISLIKES);
-        for (String dislikeEntry : youtubeEntry.getDislikes()) {
-            DefaultMutableTreeNode dislike = new DefaultMutableTreeNode(dislikeEntry);
-            dislikes.add(dislike);
-        }
-        youtube.add(dislikes);
-        DefaultMutableTreeNode subscriptions = new DefaultMutableTreeNode(Lang.SUBSCRIPTIONS);
-        for (Subscription subscriptionEntry : youtubeEntry.getSubscriptions()) {
-            DefaultMutableTreeNode subscription = new DefaultMutableTreeNode(subscriptionEntry.getSnippet().getResourceId().getChannelId());
-            subscriptions.add(subscription);
-        }
-        youtube.add(subscriptions);
-        DefaultMutableTreeNode playlists = new DefaultMutableTreeNode(Lang.PLAYLISTS);
-        for (PlaylistElement playlistEntry : youtubeEntry.getPlaylists()) {
-            DefaultMutableTreeNode playlist = new DefaultMutableTreeNode(playlistEntry.getPlaylist().getSnippet().getTitle());
-            for (PlaylistItem itemEntry: playlistEntry.getItems()) {
-                DefaultMutableTreeNode item = new DefaultMutableTreeNode(itemEntry.getSnippet().getResourceId().getVideoId());
-                playlist.add(item);
-            }
-            playlists.add(playlist);
-        }
-        youtube.add(playlists);
 
-        return new JTree(youtube);
-    }
+
 
 }
